9deffa903e176227eb492ece7dd3139ea22042d8
Alejandro Muñoz <alex12munozgeo@hotmail.es>
2022-06-05 19:33:09 +0200

    add menu and header styles

--- dist/components/Header.js
+++ dist/components/Header.js
@@ -10,8 +10,12 @@ export class Header extends Component {
     }
     createTemplate() {
         return `
-          <header>
-            <h1>PokeAPI - Alejandro Muñoz</h1>
+          <header class='header-app'>
+            <h1 class='header-app__title'>PokeAPI - Alejandro Muñoz</h1>
+            <ul class='header-app__list'>
+              <li><a href='#'>Home</a></li>
+              <li><a href='/favs'>Pokémons Favoritos</a></li>
+            </ul>
           </header>
     `;
     }
